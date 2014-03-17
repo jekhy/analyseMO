d04e2c144866d8b2198e6e514f7daf1570583842
jekhy <tansuanna@gmail.com>
2014-03-17 17:22:29 +0800

    some changes
    fixed endian bug
    append function "mo2po"

--- analyseMO.php
+++ analyseMO.php
@@ -6,13 +6,22 @@
  *
  * @auther jekhy(info@jekhy.com)
  */
+
+/**
+ * 分析MO文件 
+ *
+ * @param string $moFile MO 文件路径
+ *
+ * @return StdClass
+ */
 function analyseMO($mofile)
 {
     $mo = file_get_contents($mofile);
-    $magicNumber = trim(reset(unpack("h*", substr($mo, 0, 4))));
+    $magicNumbers = unpack("h*", substr($mo, 0, 4));
+    $magicNumber = trim(reset($magicNumbers));
 
     // N:big endian, V:little endian
-    $endian = $magicNumber == "de120495" ? "V" : "N";
+    $endian = $magicNumber == "de120495" ? "N" : "V";
     $header = (object)unpack(implode("/", array(
         "{$endian}revision",
         "{$endian}total",
@@ -39,3 +48,35 @@ function analyseMO($mofile)
     );
     return $result;
 }
+
+/**
+ * 根据MO文件生成PO文件
+ *
+ * @param string $moPath MO文件路径
+ * @param string $poPath PO文件路径
+ *
+ * @return void
+ */
+function mo2po($moPath, $poPath)
+{
+    $r = analyseMO('./common.mo');
+    $output = "";
+    foreach ($r->data as $i => $row) {
+        $id  = $row->original;
+        $str = $row->translation;
+        if ($i == 0) {
+            $str = '';
+        }
+        $output .= "msgid \"{$id}\"\n";
+        $output .= "msgstr \"{$str}\"\n";
+        if ($i == 0) {
+            $headInfo = array_map("addslashes", explode("\n", trim($row->translation)));
+            $output .= '"' . implode("\\n\"\n\"", $headInfo) . "\\n\"\n";
+        }
+        $output .= "\n";
+    }
+    file_put_contents($poPath, $output);
+}
+
+// usage:
+// mo2po('input.mo', 'output.po');
